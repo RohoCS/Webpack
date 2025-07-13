3eb3ebbbcd8cdf2661f768cb62048f332093e277
RohoCS <notand21@gmail.com>
2025-07-13 14:20:04 +0300

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: "[name].[contenthash].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   resolve: {
     extensions: ['.js', '.json', '.png'],
